82bf1d815bc3f0ec5512fcbcfe93df3c6b41b1a3
Norah.Waswala <norah.waswala@student.moringa.com>
2022-06-01 10:41:20 +0300

    include filter by location decorator

--- images/views.py
+++ images/views.py
@@ -15,7 +15,7 @@ def landing(request):
 
 def single(request,images_id):
     try:
-        single = single.objects.get(id = images_id)
+        single = Image.objects.get(id = images_id)
     except Image.DoesNotExist:
         raise Http404()
     return render(request,"single.html", {"single":single})
@@ -24,3 +24,14 @@ def image_location(request,location):
     images = Image.filter_by_location(location)
     print(images)
     return render(request, 'location.html', {'location_images': images})
+
+def search_results(request):
+    if 'imagesearch' in request.GET and request.GET["imagesearch"]:
+        category = request.GET.get("imagesearch")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
+        print(searched_images)
+        return render(request, 'search.html', {"message": message, "images": searched_images})
+    else:
+        message = "You haven't searched for any image category"
+        return render(request, 'search.html', {"message": message})
